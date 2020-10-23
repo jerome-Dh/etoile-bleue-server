b707049691df856f6fbe2223656dc37aa7ca05c5
jeromedh <jdieuhou@yahoo.fr>
2020-10-23 23:02:02 +0100

    Matrix product

---
+++ headers/matrice-mpi.h
@@ -0,0 +1,19 @@
+
+/**
+ * Perform computing of product of matrixes with MPI
+ * We use here MPI and OMP
+ * 
+ * @author Jerome Dh
+ * @date 24/09/2020
+ */
+
+#ifndef MATRICE_MPI_H
+#define MATRICE_MPI_H
+
+#define T1  800 // Taille de la matrice sur x
+#define T2  800 // Taille de la matrice sur y
+#define RESULT_FILENAME     "www/result.txt"
+#define OUT_FILENAME        "www/out.txt"
+#define OUTPUT_MATRIX_NAME  "./matrice"
+
+#endif // MATRICE_MPI_H
